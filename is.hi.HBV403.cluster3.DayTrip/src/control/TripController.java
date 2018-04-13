7068d4ad06bf89ff0e37d0a87c5d6a44ecdb133e
Audunn Andri Olafsson <aao7@hi.is>
2018-04-13 14:33:46 +0000

    trip controller forritadur

--- is.hi.HBV403.cluster3.DayTrip/src/control/TripController.java
+++ is.hi.HBV403.cluster3.DayTrip/src/control/TripController.java
@@ -8,21 +8,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TripController {
-    private List<Trip> trips;
-
     public TripController() {
     }
 
-    public List<Trip> searchTrips(Filter filter){
-        return null;
+    public List<Trip> searchTrips(Filter filter)
+    {
+        List<Trip> allTrips = DBManager.getAllTrips();
+        List<Trip> trips =  new ArrayList<>();
+
+        for(Trip trip : allTrips) {
+            if(filter.coupleFriendly != null && filter.coupleFriendly != trip.coupleFriendly) continue;
+            if(filter.familyFriendly != null && filter.familyFriendly != trip.coupleFriendly) continue;
+            if(filter.groupFriendly != null && filter.groupFriendly != trip.groupFriendly) continue;
+            if(filter.wheelChairAccess != null && filter.wheelChairAccess != trip.wheelchairAccess) continue;
+            if(filter.tripDifficulty != null && filter.tripDifficulty > trip.tripDifficulty) continue;
+            if(filter.priceMax != null && filter.priceMax < trip.tripPrice) continue;
+            if(filter.priceMin != null && filter.priceMin > trip.tripPrice) continue;
+            if(filter.tripStartDate != null && filter.tripStartDate.after(trip.tripStartDate)) continue;
+            if(filter.tripEndDate != null && filter.tripEndDate.before(trip.tripEndDate)) continue;
+            trips.add(trip);
+        }
+        return trips;
     }
     public Trip getTrip(Long tripID) {
         return DBManager.getTrip(tripID);
     }
-    public List<Trip> showTrips() {
-        return this.trips;
-    }
-    public void getAllTrips(){
-        this.trips =  DBManager.getAllTrips();
+    public List<Trip> getAllTrips(){
+        return DBManager.getAllTrips();
     }
 }
--- is.hi.HBV403.cluster3.DayTrip/src/model/Filter.java
+++ is.hi.HBV403.cluster3.DayTrip/src/model/Filter.java
@@ -1,4 +1,20 @@
 package model;
 
+import java.util.Date;
+
 public class Filter {
+    public Date tripStartDate;
+    public Date tripEndDate;
+    public Float priceMin;
+    public Float priceMax;
+    public Integer tripDifficulty;
+    public Boolean wheelChairAccess;
+    public Boolean familyFriendly;
+    public Boolean coupleFriendly;
+    public Boolean groupFriendly;
+    public String location;
+
+    public Filter(){
+    }
+
 }
